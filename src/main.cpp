378f614bc5d464e08f2ed866416b676072e93325
Bruno Borchardt <bruno.borchardt@tuhh.de>
2020-07-09 11:04:18 +0200

    main output namen standadisiert und in svg ordner getan

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ int main()
 		const Monomials m_2({ 0, 0, 1, 0, 0 });
 		const Monomials m_3({ 0, 0, 0, 1, 0 });
 		const Monomials m_4({ 0, 0, 0, 0, 1 });
-		Plot plot("test_m4.svg", 2, 2);
+		Plot plot("svg/monomials_base.svg", 2, 2);
 		plot.add_polynomial(m_0, RGB{ 0, 0, 0 });
 		plot.add_polynomial(m_1, RGB{ 0, 50, 0 });
 		plot.add_polynomial(m_2, RGB{ 0, 100, 0 });
@@ -32,7 +32,7 @@ int main()
 		const Monomials p2 = no_root_multiplicities(p);
 		auto root_intervals = descartes_root_isolation(p, Interval{ -60, 150 });
 
-		Plot plot("test_plot.svg", 5, 5);
+		Plot plot("svg/monomials_roots.svg", 5, 5);
 		plot.add_polynomial(p2, rgb::navy);
 		plot.add_polynomial(p, rgb::powder_blue);
 		for (const auto& interval : root_intervals) {
@@ -48,12 +48,12 @@ int main()
 		const Bernstein b_2({ 0, 0, 1, 0, 0 }, { 0, 10 });
 		const Bernstein b_3({ 0, 0, 0, 1, 0 }, { 0, 10 });
 		const Bernstein b_4({ 0, 0, 0, 0, 1 }, { 0, 10 });
-		Plot plot2("test_b4.svg", 10, 2);
-		plot2.add_polynomial(b_0, RGB{ 0, 0, 0 });
-		plot2.add_polynomial(b_1, RGB{ 0, 50, 0 });
-		plot2.add_polynomial(b_2, RGB{ 0, 100, 0 });
-		plot2.add_polynomial(b_3, RGB{ 0, 150, 0 });
-		plot2.add_polynomial(b_4, RGB{ 0, 200, 0 });
+		Plot plot("svg/bernstein_base.svg", 10, 2);
+		plot.add_polynomial(b_0, RGB{ 0, 0, 0 });
+		plot.add_polynomial(b_1, RGB{ 0, 50, 0 });
+		plot.add_polynomial(b_2, RGB{ 0, 100, 0 });
+		plot.add_polynomial(b_3, RGB{ 0, 150, 0 });
+		plot.add_polynomial(b_4, RGB{ 0, 200, 0 });
 		std::cout << "  done\n";
 	}
 
@@ -62,14 +62,14 @@ int main()
 		const Bernstein b({ -4, 4, 4,-4, -4, 4, 4, -4, -4, 4 }, { -5, 5 });
 		const auto [b1, b2] = de_casteljau_split(b);
 		auto roots = descartes_root_isolation(b);
-		Plot plot3("test_bernstein.svg", 5, 5);
-		plot3.add_polynomial(b, rgb::red);
-		plot3.add_control_polygon(b, svg_style::line(rgb::blue, 0.5));
-		plot3.add_control_polygon(b1, svg_style::line(rgb::red, 0.3));
-		plot3.add_control_polygon(b2, svg_style::line(rgb::blue, 0.3));
+		Plot plot("svg/bernstein_roots.svg", 5, 5);
+		plot.add_polynomial(b, rgb::red);
+		plot.add_control_polygon(b, svg_style::line(rgb::blue, 0.5));
+		plot.add_control_polygon(b1, svg_style::line(rgb::red, 0.3));
+		plot.add_control_polygon(b2, svg_style::line(rgb::blue, 0.3));
 
 		for (const auto& interval : roots) {
-			plot3.add_interval(interval);
+			plot.add_interval(interval);
 		}
 		std::cout << "  done\n";
 	}
